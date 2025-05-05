66b802154c0f019b4e433091d86c3609aef96e33
xyfruux <victor.stampe2010@gmail.com>
2025-05-05 14:29:20 +0200

    Create Style

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const port = 3000;
 const usersRouter = require("./routes/users.js");
 app.use("/users", usersRouter);
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
