792e9cfac57a4436c2420ddbc498e71d8350466b
xyfruux <victor.stampe2010@gmail.com>
2025-05-05 15:04:16 +0200

    Update Template + Title

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
 const port = 3000;
 
@@ -6,15 +7,16 @@ const usersRouter = require("./routes/users.js");
 app.use("/users", usersRouter);
 
 app.use(express.static("public"));
+app.use(expressLayouts);
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { title: "Home" });
 });
 
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { title: "404 Not Found" });
 });
 
 app.listen(port, () => {
