91d4c282737b39bd8ec86d0925c982343dc84c87
xyfruux <victor.stampe2010@gmail.com>
2025-05-08 22:04:40 +0200

    Fix Test Router & Update Test Layout

--- routes/test.js
+++ routes/test.js
@@ -2,8 +2,10 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const router = express.Router();
 
+router.use(expressLayouts);
+
 router.get("/test-layout", (req, res) => {
-  res.render("test", { title: "Test Layout", variable: "test" });
+  res.render("test", { title: "Test Layout", console: "testtest" });
 });
 
 module.exports = router;
