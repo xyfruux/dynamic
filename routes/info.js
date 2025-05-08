3c073c1f008db81e294fac5bca4a1247adc732a6
xyfruux <victor.stampe2010@gmail.com>
2025-05-08 15:02:17 +0200

    Create About Us Ejs File & Fix Template

--- routes/info.js
+++ routes/info.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const router = express.Router();
 
+router.use(expressLayouts);
+
 router.get("/about", (req, res) => {
-  res.send("About Us");
+  res.render("about", { title: "About Us" });
 });
 router.get("/contact", (req, res) => {
   res.send("Contact Us");
